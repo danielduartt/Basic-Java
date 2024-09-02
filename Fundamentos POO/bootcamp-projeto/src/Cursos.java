67badec434e400357e562936c52026c06ccd8c81
Daniel <dev.daniel.duarte@gmail.com>
2024-09-02 15:13:29 -0300

    Normalize line endings

---
+++ Fundamentos POO/bootcamp-projeto/src/Cursos.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Cursos {
+    private String titulo;
+    private String descricao;
+    private int cargoHoraria;
+}
---
+++ Fundamentos POO/bootcamp-projeto/src/Mentorias.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+
+public class Mentorias {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Mentorias(String titulo, String descricao,LocalDate data){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data = data;
+    }
+
+// ---------------------------------------------------------------------------------------
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
